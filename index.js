587b4e63c9d41f2dd835d26e2bb84f182db8bfed
Alejo Boga <alejoboga20@gmail.com>
2021-09-06 10:11:43 -0300

    Serving public directory with express

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const app = require('express');
+const express = require('express');
+
+const app = express();
 const server = require('http').createServer(app);
 
 const io = require('socket.io')(server);
 
-io.on('connection', () => {});
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', () => console.log('Client Connected'));
 server.listen(8080, () => console.log('server up'));
